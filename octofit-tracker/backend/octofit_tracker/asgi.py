932a252018cc4433526bdfd9b9ad23e86de743c0
AjmalRasouli <ajrasouli@hotmail.com>
2025-04-08 19:31:40 +0000

    wip

--- octofit-tracker/backend/octofit_tracker/asgi.py
+++ octofit-tracker/backend/octofit_tracker/asgi.py
@@ -6,7 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
-
+# This is a sample code snippet for an ASGI configuration file in a Django project.
 import os
 
 from django.core.asgi import get_asgi_application
